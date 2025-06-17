eded3720f0e6be81d214bd82e415d98cac0d69ab
AGhebrea <alex.ghebrea4@gmail.com>
2025-06-17 21:37:23 +0300

    bugfix

--- src/rrrr.sh
+++ src/rrrr.sh
@@ -6,14 +6,17 @@ if [ "$#" -eq 0 ]; then
 fi
 
 RRTARGET=$(basename "$1")
-RRFILE=/home/alex/.local/share/rr/${RRTARGET}-0
+RRDIRBASE=/home/alex/.local/share/rr/${RRTARGET}
+RRDIR=${RRDIRBASE}-0
 
 randomize_va_space=$(cat /proc/sys/kernel/randomize_va_space)
-echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
-if -f ${RRFILE}; then
-    rm ${RRFILE}
-fi
-rr record ${RRTARGET}
-echo $randomize_va_space | sudo tee /proc/sys/kernel/randomize_va_space
+echo 0 | sudo tee /proc/sys/kernel/randomize_va_space &> /dev/null
+for DIR in $RRDIRBASE-*; do
+    if [ -d ${DIR} ] ; then
+        rm -rf ${DIR}
+    fi
+done
+rr record "$1"
+echo $randomize_va_space | sudo tee /proc/sys/kernel/randomize_va_space &> /dev/null
 clear
-rr replay ${RRFILE}
+rr replay ${RRDIR}
