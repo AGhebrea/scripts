14267ec36b8d746bdbf6109bac3f2c8cf2d82781
AGhebrea <alex.ghebrea4@gmail.com>
2025-06-17 21:25:46 +0300

    added install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+# alias entest='source entest.sh'
+# alias rrrr='rrrr.sh'
+
+remindSource=0
+
+for file in ./src/*.sh; do
+    bname=$(basename $file)
+    bnameStripped="${bname%.*}"
+    if [ ! -L "/usr/bin/$bname" ]; then
+        sudo ln -s "$PWD/src/$bname" "/usr/bin/$bname"
+    fi
+    grep "alias $bnameStripped=" ~/.bashrc &>/dev/null
+    if [ $? != 0 ]; then
+        remindSource=1
+        echo "alias $bnameStripped=$bname" >> ~/.bashrc
+    fi
+done
+
+if [ $remindSource -eq 1 ]; then
+    echo "Don't forget to source ~/.bashrc"
+fi
