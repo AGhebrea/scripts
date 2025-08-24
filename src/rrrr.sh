0259a505a21c98030a624ac803432f657b89b264
AGhebrea <alex.ghebrea4@gmail.com>
2025-08-24 09:06:53 +0300

    added --last to rrrr

--- src/rrrr.sh
+++ src/rrrr.sh
@@ -3,6 +3,7 @@
 ARGS="$@"
 FIRST=""
 SECOND=""
+RUNLAST=0
 
 if [[ $ARGS == *' -- '* ]]; then
     FIRST="${ARGS% --*}"
@@ -10,27 +11,32 @@ if [[ $ARGS == *' -- '* ]]; then
 else
     FIRST=${ARGS}
 fi
-
 if [ "$#" -eq 0 ]; then
-    printf 'Usage: \n\t%s <executable>\n' $0
+    printf 'Usage: \n\t%s <OPTIONS> <executable> -- <rr-replay-arguments (essentially gdb-arguments)>\n' $0
+    echo 'OR'
+    printf 'Usage: \n\t%s <OPTIONS> --last -- <rr-replay-arguments (essentially gdb-arguments)>\n' $0
     exit 1
 fi
-
 read -ra args <<< "${FIRST}"
-RRTARGET="${args[0]}"
-
+if [ ${args[0]} == "--last" ]; then
+    RRTARGET="${args[1]}"
+    RUNLAST=1
+else
+    RRTARGET="${args[0]}"
+fi
 RRTARGETBASENAME=$(basename $RRTARGET)
 RRDIRBASE=${HOME}/.local/share/rr/${RRTARGETBASENAME}
 RRDIR=${RRDIRBASE}-0
 RRARGS="${args[@]:1}"
-
 randomize_va_space=$(cat /proc/sys/kernel/randomize_va_space)
-echo 0 | sudo tee /proc/sys/kernel/randomize_va_space &> /dev/null
-for DIR in $RRDIRBASE-*; do
-    if [ -d ${DIR} ] ; then
-        rm -rf ${DIR}
-    fi
-done
-rr record ${RRTARGET} ${RRARGS}
-echo $randomize_va_space | sudo tee /proc/sys/kernel/randomize_va_space &> /dev/null
+if [ ${RUNLAST} == 0 ]; then
+    echo 0 | sudo tee /proc/sys/kernel/randomize_va_space &> /dev/null
+    for DIR in $RRDIRBASE-*; do
+        if [ -d ${DIR} ] ; then
+            rm -rf ${DIR}
+        fi
+    done
+    rr record ${RRTARGET} ${RRARGS}
+    echo $randomize_va_space | sudo tee /proc/sys/kernel/randomize_va_space &> /dev/null
+fi
 rr replay ${RRDIR} ${SECOND}
