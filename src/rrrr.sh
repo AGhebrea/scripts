07c8278b17163c3c75ea8f0a6a90d390ccfa9496
AGhebrea <alex.ghebrea4@gmail.com>
2025-10-26 08:09:39 +0200

    feature: rrrr script option to automatically continue at start

--- src/rrrr.sh
+++ src/rrrr.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/bash
 
+parse_args(){
+    set -- $FIRST
+    OPTIND=1
+    while getopts ":lct:" opt; do
+        case "$opt" in
+            l) RUNLAST=1 ;;
+            c) RUNCONTINUE=1 ;;
+            t) RRTARGET="$OPTARG" ;;
+        esac
+    done
+}
+
 ARGS="$@"
 FIRST=""
 SECOND=""
 RUNLAST=0
+RUNCONTINUE=0
+RRTARGET=""
 
 if [[ $ARGS == *' -- '* ]]; then
     FIRST="${ARGS% --*}"
@@ -11,19 +25,13 @@ if [[ $ARGS == *' -- '* ]]; then
 else
     FIRST=${ARGS}
 fi
+parse_args
 if [ "$#" -eq 0 ]; then
-    printf 'Usage: \n\t%s <OPTIONS> <executable> -- <rr-replay-arguments (essentially gdb-arguments)>\n' $0
-    echo 'OR'
-    printf 'Usage: \n\t%s <OPTIONS> --last <executable> -- <rr-replay-arguments (essentially gdb-arguments)>\n' $0
+    printf 'Usage: \n\t%s [-l] [-c] -t <executable> -- <rr-replay-arguments (essentially gdb-arguments)>\n' $0
+    printf 'Where: \n\t-c: automatically inputs the char "c" in STDIN\n\t-l: runs last recording made with rrrr\n'
     exit 1
 fi
 read -ra args <<< "${FIRST}"
-if [ ${args[0]} == "--last" ]; then
-    RRTARGET="${args[1]}"
-    RUNLAST=1
-else
-    RRTARGET="${args[0]}"
-fi
 RRTARGETBASENAME=$(basename $RRTARGET)
 RRDIRBASE=${HOME}/.local/share/rr/${RRTARGETBASENAME}
 RRDIR=${RRDIRBASE}-0
@@ -39,4 +47,8 @@ if [ ${RUNLAST} == 0 ]; then
     rr record ${RRTARGET} ${RRARGS}
     echo $randomize_va_space | sudo tee /proc/sys/kernel/randomize_va_space &> /dev/null
 fi
-rr replay ${RRDIR} ${SECOND}
+if [[ ${RUNCONTINUE} == 1 ]]; then
+    (echo -e "c\n"; cat /dev/tty) | rr replay ${RRDIR} ${SECOND}
+else
+    rr replay ${RRDIR} ${SECOND}
+fi
