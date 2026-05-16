dc6f29e4899c143960067924a0b93f99ba8864f6
AGhebrea <alex.ghebrea4@gmail.com>
2026-05-16 06:27:45 +0300

    tdbg bugfix

--- src/tdbg.sh
+++ src/tdbg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tmux new-session -d -s pwndbg_panes 2>/dev/null || true
-if [[ $# -ne 1 && $1 != "--" ]]; then
+if [[ $1 != "--" ]]; then
     tmux send-keys -t pwndbg_panes "gdb $*" Enter
 fi
 tmux attach -t pwndbg_panes
